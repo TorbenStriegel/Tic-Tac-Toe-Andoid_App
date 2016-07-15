1da28f6bfc7eafe8da446a2998ff0f6e86f860c3
Torben Striegel <Torben Striegel>
2016-07-15 18:12:08 +0200

    Button Classe erweitert.

--- Android_App/TicTacToe_AndroidAplication/app/src/main/java/de/torbennils/tictactoe/Buttons_Funktionen.java
+++ Android_App/TicTacToe_AndroidAplication/app/src/main/java/de/torbennils/tictactoe/Buttons_Funktionen.java
@@ -1,15 +1,23 @@
 package de.torbennils.tictactoe;
 
+import android.graphics.Color;
 import android.widget.Button;
 
 public class Buttons_Funktionen {
 
     private Button[] buttons;
-    private int buttonAnzahl;
+    private int buttonAnzahl = 0;
+    private Main_Activity main_activity = null;
 
-    Buttons_Funktionen(int anzahlButtons, Button[] buttonArray){
+    public Buttons_Funktionen(Main_Activity main_activity,int anzahlButtons, Button[] buttonArray){
+        this.main_activity = main_activity;
         this.buttonAnzahl = anzahlButtons;
         this.buttons = buttonArray;
+    }
+    public void setFarbe(Button[] buttonAnmalen, int buttonFarbe){
+     for (int i =0;i < buttonAnmalen.length;i++){
+         buttonAnmalen[i].setBackgroundColor(buttonFarbe);
+        }
     }
 
     public Button[] getButtonArray(){
